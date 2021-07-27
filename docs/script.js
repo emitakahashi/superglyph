25030b1aadd2febc01cde063191976920806ea8e
Emi Takahashi <emitakahashi@users.noreply.github.com>
2021-07-27 00:15:24 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,11 @@
 let weightSlider = document.getElementById("weight-slider");
 
-let fontWeight = document.getElementById("font");
+let fontWeight = document.getElementById("font", "fonttwo");
 
 weightSlider.oninput = function() {
   fontWeight.style.setProperty('font-weight',weightSlider.value.toString()) 
 }
+
+
+
+
