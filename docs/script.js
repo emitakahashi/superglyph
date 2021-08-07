21cd868f665d734ee4c3d0da642dedb35af1cb96
Emi Takahashi <emitakahashi@users.noreply.github.com>
2021-08-06 23:33:09 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -43,13 +43,5 @@ weightSlider.oninput = function() {
 
 
 
-$(document).ready(function(){
-    $('#para').hover(function(){
-        $(this).addClass('blur');
-    }).mouseout(function(){
-        $(this).removeClass('blur');
-    });
-});
-
 
 
